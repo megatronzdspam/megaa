ddb8799fa3bf389b07737b49c7e13284bd055fd0
megatronzdspam <bot.zdspam@gmail.com>
2016-01-05 15:12:43 +0330

    Create version.lua

---
+++ plugins/version.lua
@@ -0,0 +1,15 @@
+do
+
+function run(msg, matches)
+  return 'megatron '.. VERSION .. [[ 
+  
+  amir/amir/abas
+ 
+end
+return {
+    patterns = {
+    "^[!/]([Vv]ersion)$"
+  }, 
+  run = run 
+}
+end
