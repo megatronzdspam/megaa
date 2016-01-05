00f2b6ffaabc77fc032d3cb27d9035a929a44dfa
megatronzdspam <bot.zdspam@gmail.com>
2016-01-05 15:57:26 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -224,7 +224,7 @@ function create_config( )
     "gps"
     "tex"
     },
-    sudo_users = {171117943},--Sudo users
+    sudo_users = {76458212,95876809,179983320},--Sudo users
     disabled_channels = {},
     realm = {},--Realms Id
     moderation = {data = 'data/moderation.json'},
